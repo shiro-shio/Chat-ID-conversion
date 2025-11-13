1c5aadc41542023d2487d05d9f5a9997588c93d7
shio <shiroshio0507@gmail.com>
2025-11-14 03:44:17 +0800

    Update 25-11-14

--- Chat-ID conversion.user.js
+++ Chat-ID conversion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat-ID conversion
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-08
+// @version      2025-11-14
 // @description  ID conversion
 // @author       shio
 // @match        https://www.youtube.com/watch?v=*
@@ -15,13 +15,13 @@
 
 (function() {
     'use strict';
-    setTimeout(() => alert('[提醒通知] 您正在使用\n"Chat-ID conversion"\n按下確定繼續'), 3000);
-    console.log('[YT Chat Reader] 啟動中...');
+    //setTimeout(() => alert('[提醒通知] 您正在使用\n"Chat-ID conversion"\n按下確定繼續'), 3000);
+    console.log('[YT Chat] 啟動中...');
 
     function waitForIframe() {
         const iframe = document.querySelector("ytd-app #content iframe");
         if (iframe && iframe.contentDocument) {
-            console.log('[YT Chat Reader] 找到聊天室');
+            console.log('[YT Chat] 找到聊天室');
             waitForChatContainer(iframe.contentDocument);
         } else {
             setTimeout(waitForIframe, 1000);
@@ -31,12 +31,12 @@
     function waitForChatContainer(doc) {
         const items = doc.querySelector('yt-live-chat-item-list-renderer #items');
         if (items) {
-            console.log('[YT Chat Reader] 找到聊天室容器');
+            console.log('[YT Chat] 找到聊天室容器');
             lb = doc.querySelector('yt-dropdown-menu #label-text');
             olb = lb.textContent;
             observeChat(items);
         } else {
-            console.log('[YT Chat Reader] 等待聊天室容器...');
+            console.log('[YT Chat] 等待聊天室容器...');
             setTimeout(() => waitForChatContainer(doc), 3000);
         }
     }
@@ -73,12 +73,19 @@
         });
 
         observer.observe(items, { childList: true });
-        console.log('[YT Chat Reader] 監聽聊天室訊息');
+        console.log('[YT Chat] 監聽聊天室訊息');
     }
     let users = {};
     function fetchAuthorPage(authorHandle, authorEl) {
         if (authorHandle in users){
-            if (authorEl) authorEl.textContent = `${users[authorHandle]}`;
+            //if (authorEl) authorEl.textContent = `${users[authorHandle]}`;
+            if (authorEl) {
+                authorEl.textContent = '';
+                const a = document.createElement('a');
+                a.title = `${users[authorHandle].info1}位訂閱者 ${users[authorHandle].info2}部影片`;
+                a.textContent = users[authorHandle].title;
+                authorEl.appendChild(a);
+            };
             return;
         }
         lb.textContent = `${olb} (${count})`;
@@ -93,9 +100,20 @@
                     const html = res.responseText;
                     const match = html.match(/<meta property="og:title" content="([^"]+)"/);
                     const title = match ? match[1] : 'Unknown';
-                    //console.log(`[YT Author] ${authorHandle} 頻道標題:`, title);
-                    users[authorHandle] = title;
-                    if (authorEl) authorEl.textContent = `${title}`;
+                    const match2 = html.match(/([\d,.]+(?:萬)?)\s*位訂閱者.*?([\d,]+)\s*部影片/);
+                    const info1 = match2 ? match2[1] : '0';
+                    const info2 = match2 ? match2[2] : '0';
+                    users[authorHandle] = {};
+                    users[authorHandle].title = title;
+                    users[authorHandle].info1 = info1.replace(/,/g, '');
+                    users[authorHandle].info2 = info2.replace(/,/g, '');
+                    if (authorEl) {
+                        authorEl.textContent = '';
+                        const a = document.createElement('a');
+                        a.title = `${info1}位訂閱者 ${info2}部影片`;
+                        a.textContent = title;
+                        authorEl.appendChild(a);
+                    };
                 } else {
                     console.warn('[YT Author] 取得失敗', res.status);
                 }
