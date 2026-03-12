867d19459a754b9835bd48b109f2db33135ec653
shio <shiroshio0507@gmail.com>
2026-03-12 23:30:58 +0800

    Update Chat-ID conversion.user.js

--- Chat-ID conversion.user.js
+++ Chat-ID conversion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Chat-ID conversion
+// @name         Chat-ID conversion V2
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-22
+// @version      2026-03-13
 // @description  ID conversion
 // @author       shio
 // @match        https://www.youtube.com/watch?v=*
@@ -92,40 +92,19 @@
         }
         lb.textContent = `${olb} (${count})`;
         count += 1
-        const url = `https://www.youtube.com/${authorHandle}`;
+        const url = `https://yt-id-conversion.shiroshio0507.workers.dev/?chid=${authorHandle}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+            if (data.status === "success") {
+                users[authorHandle] = data.name;
+                if (authorEl) authorEl.textContent = `${data.name}`;
+            } else {
+                if (authorEl) authorEl.textContent = `${authorHandle}`;
+            }
+        })
+            .catch(err => console.error("請求失敗:", err));
 
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: url,
-            onload: res => {
-                if (res.status === 200) {
-                    const html = res.responseText;
-                    //console.log(html)
-                    const match = html.match(/<meta property="og:title" content="([^"]+)"/);
-                    const title = match ? match[1] : 'Unknown';
-                    users[authorHandle] = title;
-                    if (authorEl) authorEl.textContent = `${title}`;
-                    /*const match2 = html.match(/([\d,.]+(?:萬)?)\s*位訂閱者.*?([\d,]+)\s*部影片/);
-                    const info1 = match2 ? match2[1] : '?';
-                    const info2 = match2 ? match2[2] : '?';*/
-                    //console.log(match2)
-                    /*users[authorHandle] = {};
-                    users[authorHandle].title = title;
-                    users[authorHandle].info1 = info1;
-                    users[authorHandle].info2 = info2;
-                    if (authorEl) {
-                        authorEl.textContent = '';
-                        const a = document.createElement('a');
-                        a.title = `${info1}位訂閱者 ${info2}部影片`;
-                        a.textContent = title;
-                        authorEl.appendChild(a);
-                    };*/
-                } else {
-                    console.warn('[YT Author] 取得失敗', res.status);
-                }
-            },
-            onerror: err => console.error('[YT Author] 請求錯誤', err)
-        });
     }
     waitForIframe();
 })();
